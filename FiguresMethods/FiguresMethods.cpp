bd5a92ae4241c97921f264d7a80fed0d9af5f872
Prodject <Liter.Hari@gmail.com>
2023-10-10 18:15:35 +0200

    first commit

--- FiguresMethods/FiguresMethods.cpp
+++ FiguresMethods/FiguresMethods.cpp
@@ -3,149 +3,548 @@
 class  Figures
 {
 public:
-	int A, B, C, D; //углы 
-	int a, c, b, d; //стороны 	
+	std::string NameFigures;
 
 	Figures() {}
-	Figures(int a, int b, int c, int A, int B, int C) {
-		std::cout << "Стороны: " << "a=" << a << " b=" << b << " c=" << c << std::endl;
-		std::cout << "Углы: " << "A=" << A << " B=" << B << " C=" << C << std::endl;
+	Figures(int sizeArray,int sizeSide[],int sizeAngles[],char abcSide[], char abcAngles[]) {
+		for (int i = 0;i < sizeArray;i++) {
+			std::cout << abcSide[i] << " = ";
+			std::cout << sizeSide[i] << " ";
+		}	
+		std::cout << std::endl;
+		for (int i = 0;i < sizeArray;i++) {			
+			std::cout << abcAngles[i] << " = ";
+			std::cout << sizeAngles[i] << " ";
+		}
 		std::cout << std::endl;
 	}
-	Figures(int a, int b, int c, int d, int A, int B, int C, int D) {
-		std::cout << "Стороны: " << "a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-		std::cout << "Углы: " << "A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
-		std::cout << std::endl;
-	}	
-	
-	virtual void PrintInfoFigures(int counternumberSides,int ANumber, int BNumber, int CNumber,int DNumber, 
-		std::string NameFigures)
-	{		
-		std::cout << NameFigures << std::endl;
 
-		if (counternumberSides == 0) {
-			std::cout << "Правильная" << std::endl;		
-		}
-		if (counternumberSides == 3) {
-			if (ANumber - 10 == BNumber && 90 == CNumber) {
-				std::cout << "Правильная" << std::endl;
-			}
-			if (ANumber + 10 == BNumber && 90 == CNumber) {
-				std::cout << "Неправильная" << std::endl;
-			}
-			if (ANumber + 10 == BNumber && BNumber + 10 == CNumber) {
-				std::cout << "Правильная" << std::endl;
-			}
-			if (ANumber == BNumber && BNumber == CNumber) {
-				std::cout << "Правильная" << std::endl;
-			}				
-			if (ANumber < BNumber && BNumber > CNumber || ANumber == CNumber && CNumber < BNumber) {
-				std::cout << "Неправильная" << std::endl;
-			}
-		}
-		if (counternumberSides == 4) {
-			if (ANumber + 10 == BNumber && BNumber + 10 == CNumber && CNumber + 10 == DNumber) {
-				std::cout << "Неправильная" << std::endl;
-			}
-			if (ANumber + 10 == BNumber && BNumber - 10 == CNumber && CNumber == DNumber - 10) {
-				std::cout << "Неправильная" << std::endl;
-			}
-			if (ANumber == BNumber &&  CNumber == DNumber) {
-				std::cout << "Правильная" << std::endl;
-			}
-		}
-		std::cout << "Количество сторон: " << counternumberSides << std::endl;	
-	}	
+	virtual std :: string getType() {
+		return NameFigures;
+	}
 };
 
-class UnknownFigures : public Figures {
-public:
-};
+void print_info(Figures* f) {
+	std :: cout << f->getType() << std :: endl;
+}
+
+
 
 class  Triangle : public Figures
 {
-public:
-	Triangle() {
-		a = 10, b = 20, c = 30;
-		A = 50, B = 60, C = 70;		
-	}	
+public:	
+	static const int sizeArray = 3;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+	std::string NameFigures= "Треугольник:";
+	
+	int a = 10,b = 20,c = 30;//стороны
+	int	A = 50,B = 60,C = 70;//углы
+	//int AA, BB, CC;	
+	
+	void info(){//int a,int b,int c,int A,int B,int C) {
+		//AA = A;
+		//BB = B;
+		//CC = C;
+       //sizeArray = 3;
+	    siDe[sizeArray], Angles[sizeArray];
+	    abcSide[sizeArray],abcAngles[sizeArray];	
+	
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray,siDe, Angles,abcSide,abcAngles);
+		std::cout << std::endl;
+	}
+	//Triangle():NameFigures("Треугольник") {}
+
+	std :: string getType() override {
+		return NameFigures;
+	}
 };
 
 class Quadrilateral : public Figures {
 public:
-	Quadrilateral() {
-		a = 10, b = 20, c = 30, d = 40;
-		A = 50, B = 60, C = 70, D = 80;		
+	std::string NameFigures = "Четырехугольник:";
+
+	static const int sizeArray = 4;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+	int a = 10, b = 20, c = 30, d = 40;
+	int A = 50, B = 60, C = 70, D = 80;
+	//int AA, BB, CC, DD;
+	
+	void info(){//int a,int b,int c,int d,int A,int B,int C,int D) {
+
+		siDe[sizeArray], Angles[sizeArray];
+		abcSide[sizeArray], abcAngles[sizeArray];
+
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;
+		siDe[3] = d;
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;
+		Angles[3] = D;
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';
+		abcSide[3] = 'd';
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';
+		abcAngles[3] = 'D';
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray, siDe, Angles, abcSide, abcAngles);
+		std::cout << std::endl;
 	}	
+
+	std::string getType() override {
+		return NameFigures;
+	}
 };
 
 class RightRriangle : public Figures {
 public:
-	RightRriangle() {
-		a = 10, b = 20, c = 30;
-		A = 50, B = 60, C = 90;
-	}
-};
+	std::string NameFigures = "Прямоугольный треугольник:";
 
-class RightRriangle2 : public Figures {
-public:
-	RightRriangle2() {
-		a = 10, b = 20, c = 30;
-		A = 50, B = 40, C = 90;		
+	static const int sizeArray = 3;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+    int a = 10, b = 20, c = 30;
+	int A = 50, B = 60, C = 90;
+	//int CC;
+
+	void info(){//int a,int b,int c,int A,int B,int C) {
+	//	CC = C;
+
+		siDe[sizeArray], Angles[sizeArray];
+		abcSide[sizeArray], abcAngles[sizeArray];
+
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;		
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;		
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';		
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';		
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray, siDe, Angles, abcSide, abcAngles);
+		std::cout << std::endl;
+	}
+
+	std::string getType() override {
+		return NameFigures;
 	}
 };
 
 class IsoscelesTriangle : public Figures {
 public:
-	IsoscelesTriangle() {
-		a = 10, b = 20, c = 10;
-		A = 50, B = 60, C = 50;		
+	
+	int	a = 10, b = 20, c = 10;
+	int	A = 50, B = 60, C = 50;			
+	
+	std::string NameFigures = "Равнобедренный треугольник:";
+
+	static const int sizeArray = 3;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+	//int aa, cc;
+	//int AA,CC;
+
+	void info(){//int a, int b, int c, int A, int B, int C) {
+		/*
+		CC = C;
+		AA = A;
+		aa = a;
+		cc = c;		
+		*/
+		siDe[sizeArray], Angles[sizeArray];
+		abcSide[sizeArray], abcAngles[sizeArray];
+
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray, siDe, Angles, abcSide, abcAngles);
+		std::cout << std::endl;
 	}
+
+	std::string getType() override {
+		return NameFigures;
+	}
+
 };
 
 class EquilateralTriangle : public Figures {
 public:
-	EquilateralTriangle() {
-		a = 30, b = 30, c = 30;
-		A = 60, B = 60, C = 60;		
+	
+	int	a = 30, b = 30, c = 30;
+	int	A = 60, B = 60, C = 60;		
+	
+	std::string NameFigures = "Равносторонний треугольник:";
+
+	static const int sizeArray = 3;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+	//int CC,AA,BB;
+
+	void info(){//int a, int b, int c, int A, int B, int C) {
+		/*
+		CC = C;
+		AA = A;
+		BB = B;
+		*/
+		siDe[sizeArray], Angles[sizeArray];
+		abcSide[sizeArray], abcAngles[sizeArray];
+
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray, siDe, Angles, abcSide, abcAngles);
+		std::cout << std::endl;
+	}
+	std::string getType() override {
+		return NameFigures;
 	}
 };
 
 class Rectangle : public Figures {
 public:
-	Rectangle() {
-		a = 10, b = 20, c = 10, d = 20;
-		A = 90, B = 90, C = 90, D = 90;	
+	
+	int	a = 10, b = 20, c = 10, d = 20;
+	int	A = 90, B = 90, C = 90, D = 90;	
+	
+	std::string NameFigures = "Прямоугольник:";
+
+	static const int sizeArray = 4;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+	//int AA, BB, CC, DD;
+
+	void info(){//int a, int b, int c, int d, int A, int B, int C, int D) {
+		/*
+		AA = A;
+		BB = B;
+		CC = C;
+		DD = D;
+		*/
+		siDe[sizeArray], Angles[sizeArray];
+		abcSide[sizeArray], abcAngles[sizeArray];
+
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;
+		siDe[3] = d;
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;
+		Angles[3] = D;
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';
+		abcSide[3] = 'd';
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';
+		abcAngles[3] = 'D';
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray, siDe, Angles, abcSide, abcAngles);
+		std::cout << std::endl;
+	}
+
+	std::string getType() override {
+		return NameFigures;
 	}
 };
 
 class Square : public Figures {
 public:
-	Square() {
-		a = 20, b = 20, c = 20, d = 20;
-		A = 90, B = 90, C = 90, D = 90;		
+	
+	int	a = 20, b = 20, c = 20, d = 20;
+	int	A = 90, B = 90, C = 90, D = 90;		
+	
+	std::string NameFigures = "Квадрат:";
+
+	static const int sizeArray = 4;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+	//int AA, BB, CC, DD;
+	
+	void info(){//int a, int b, int c, int d, int A, int B, int C, int D) {
+		/*
+		AA = A;
+		BB = B;
+		CC = C;
+		DD = D;
+		*/
+		siDe[sizeArray], Angles[sizeArray];
+		abcSide[sizeArray], abcAngles[sizeArray];
+
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;
+		siDe[3] = d;
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;
+		Angles[3] = D;
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';
+		abcSide[3] = 'd';
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';
+		abcAngles[3] = 'D';
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray, siDe, Angles, abcSide, abcAngles);
+		std::cout << std::endl;
+	}	
+
+	std::string getType() override {
+		return NameFigures;
 	}
 };
 
 class Parallelogram : public Figures {
 public:
-	Parallelogram() {
-		a = 20, b = 30, c = 20, d = 30;
-		A = 30, B = 40, C = 30, D = 40;	
+	
+	int	a = 20, b = 30, c = 20, d = 30;
+	int	A = 30, B = 40, C = 30, D = 40;	
+	
+	std::string NameFigures = "Параллелограмм:";
+
+	static const int sizeArray = 4;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+	//int AA, BB, CC, DD;
+
+	void info(){//int a, int b, int c, int d, int A, int B, int C, int D) {
+		/*
+		AA = A;
+		BB = B;
+		CC = C;
+		DD = D;
+		*/
+		siDe[sizeArray], Angles[sizeArray];
+		abcSide[sizeArray], abcAngles[sizeArray];
+
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;
+		siDe[3] = d;
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;
+		Angles[3] = D;
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';
+		abcSide[3] = 'd';
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';
+		abcAngles[3] = 'D';
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray, siDe, Angles, abcSide, abcAngles);
+		std::cout << std::endl;
+	}
+
+	std::string getType() override {
+		return NameFigures;
 	}
 };
 
 class Rhombus : public Figures {
 public:
-	Rhombus() {
-		a = 30, b = 30, c = 30, d = 30;
-		A = 30, B = 40, C = 30, D = 40;
-	}	
+	
+	int	a = 30, b = 30, c = 30, d = 30;
+	int	A = 30, B = 40, C = 30, D = 40;
+	
+	std::string NameFigures = "Ромб:";
+
+	static const int sizeArray = 4;
+	int siDe[sizeArray], Angles[sizeArray];
+	char abcSide[sizeArray], abcAngles[sizeArray];
+
+	//int AA, BB, CC, DD;
+
+	void info(){//int a, int b, int c, int d, int A, int B, int C, int D) {
+		/*
+		AA = A;
+		BB = B;
+		CC = C;
+		DD = D;
+		*/
+		siDe[sizeArray], Angles[sizeArray];
+		abcSide[sizeArray], abcAngles[sizeArray];
+
+		siDe[0] = a;
+		siDe[1] = b;
+		siDe[2] = c;
+		siDe[3] = d;
+
+		Angles[0] = A;
+		Angles[1] = B;
+		Angles[2] = C;
+		Angles[3] = D;
+
+		abcSide[0] = 'a';
+		abcSide[1] = 'b';
+		abcSide[2] = 'c';
+		abcSide[3] = 'd';
+
+		abcAngles[0] = 'A';
+		abcAngles[1] = 'B';
+		abcAngles[2] = 'C';
+		abcAngles[3] = 'D';
+
+		std::cout << "Количество сторон: " << sizeArray << std::endl;
+
+		Figures(sizeArray, siDe, Angles, abcSide, abcAngles);
+		std::cout << std::endl;
+	}
+
+	std::string getType() override {
+		return NameFigures;
+	}
 };
 
 int main() {
+
 	setlocale(LC_ALL, "Russian");
 
+	Figures* triangle = new Triangle();
+	print_info(triangle);
+	Triangle t;
+	t.info();//10, 20, 30, 50, 60, 70);
+
+	Figures* quadrilateral = new Quadrilateral();
+	print_info(quadrilateral);
+	Quadrilateral q;	
+	q.info();
+	
+
+	Figures* rightRriangle = new RightRriangle();
+	print_info(rightRriangle);
+	RightRriangle r;
+	r.info();
+	
+	Figures* isoscelesTriangle = new IsoscelesTriangle();
+	print_info(isoscelesTriangle);
+	IsoscelesTriangle i;	
+	i.info();
+
+	Figures* equilateralTriangle = new EquilateralTriangle();
+	print_info(equilateralTriangle);
+	EquilateralTriangle e;	
+	e.info();
+	
+	Figures* reectangle = new Rectangle();
+	print_info(reectangle);
+	Rectangle re;	
+	re.info();
+
+	Figures* square = new Square();
+	print_info(square);
+	Square s;	
+	s.info();
+
+	Figures* parallelogram = new Parallelogram();
+	print_info(parallelogram);
+	Parallelogram p;	
+	p.info();
+
+	Figures* rhombus = new Rhombus();
+	print_info(rhombus);
+	Rhombus rh;	
+	rh.info();
+
+	/*
     UnknownFigures unknownFigures;
     Figures* f= &unknownFigures;
 	f->PrintInfoFigures(0,0,0,0,0,"Фигура: ");
@@ -200,6 +599,6 @@ int main() {
 	f = &rhombus;
 	f->PrintInfoFigures(4, f->A, f->B, f->C, f->D, "Ромб: ");
 	Figures(f->a, f->b, f->c,f->d,f->A, f->B, f->C, f->D);
-	
+	*/
 	return 0;
 }
